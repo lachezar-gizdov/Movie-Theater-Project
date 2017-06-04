0b06bdbca4a0f43eaf1f3d7c952ff5bb13f27d9d
Lucho Gizdov <olebg01@gmail.com>
2017-06-04 14:26:01 +0300

    add users into theater

--- MovieTheater/MovieTheater.Models/Theater.cs
+++ MovieTheater/MovieTheater.Models/Theater.cs
@@ -1,9 +1,18 @@
-﻿namespace MovieTheater.Models
+﻿using System.Collections.Generic;
+
+namespace MovieTheater.Models
 {
     public class Theater
     {
+        private ICollection<User> users;
+
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get { return this.users; }
+        }
     }
 }
