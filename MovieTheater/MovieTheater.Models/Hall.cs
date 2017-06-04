9b845444f2b28d16becd607442b147606d0e3add
Encho Enevski <encho.enevski@gmail.com>
2017-06-04 15:51:50 +0300

    Add new tables

---
+++ MovieTheater/MovieTheater.Models/Hall.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTheater.Models
+{
+    public class Hall
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public int Seats { get; set; }
+
+        public HallShedules HallShedule { get; set; }
+    }
+}
---
+++ MovieTheater/MovieTheater.Models/HallShedules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MovieTheater.Models
+{
+    public class HallShedules
+    {
+        public int Id { get; set; }
+
+        public string MovieName { get; set; }
+
+        public DateTime ProjectionTime { get; set; }
+    }
+}
