e5d42362e6fe733909772b650236b9926d8528b7
Lucho Gizdov <olebg01@gmail.com>
2017-06-04 15:28:49 +0300

    add theaters, users and tickets connections

--- MovieTheater/MovieTheater.Models/Theater.cs
+++ MovieTheater/MovieTheater.Models/Theater.cs
@@ -5,6 +5,13 @@ namespace MovieTheater.Models
     public class Theater
     {
         private ICollection<User> users;
+        private ICollection<Movie> movies;
+
+        public Theater()
+        {
+            this.users = new HashSet<User>();
+            this.movies = new HashSet<Movie>();
+        }
 
         public int Id { get; set; }
 
@@ -12,7 +19,27 @@ namespace MovieTheater.Models
 
         public virtual ICollection<User> Users
         {
-            get { return this.users; }
+            get
+            {
+                return this.users;
+            }
+            set
+            {
+                this.users = value;
+            }
+        }
+
+        public virtual ICollection<Movie> Movies
+        {
+            get
+            {
+                return this.movies;
+            }
+
+            set
+            {
+                this.movies = value;
+            }
         }
     }
 }
--- MovieTheater/MovieTheater.Models/User.cs
+++ MovieTheater/MovieTheater.Models/User.cs
@@ -1,11 +1,32 @@
-﻿namespace MovieTheater.Models
+﻿using System.Collections.Generic;
+
+namespace MovieTheater.Models
 {
     public class User
     {
+        private ICollection<Ticket> purchasedTickets;
+
+        public User()
+        {
+            this.purchasedTickets = new HashSet<Ticket>();
+        }
         public int Id { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public virtual ICollection<Ticket> PurchasedTickets
+        {
+            get
+            {
+                return this.purchasedTickets;
+            }
+
+            set
+            {
+                this.purchasedTickets = value;
+            }
+        }
     }
 }
