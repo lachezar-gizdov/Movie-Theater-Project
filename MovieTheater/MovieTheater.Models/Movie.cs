37f1a25744c0680677ef1bc1440ba4328acd0c08
Iskren Donev <iskren.donev@me.com>
2017-06-04 14:33:31 +0300

    Added the Movie and Ticket classes

---
+++ MovieTheater/MovieTheater.Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTheater.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public short Year { get; set; }
+    }
+}
---
+++ MovieTheater/MovieTheater.Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieTheater.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public string Hall { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public string ProjectionTime { get; set; }
+
+        public decimal Price { get; set; }
+        
+        public int Seat { get; set; }  
+    }
+}
