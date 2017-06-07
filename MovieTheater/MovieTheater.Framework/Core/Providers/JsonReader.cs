251521efbd3c0db58a3380abfcc61c909ae29f01
Encho Enevski <encho.enevski@gmail.com>
2017-06-08 01:25:16 +0300

    add JsonReader provider

---
+++ MovieTheater/MovieTheater.Framework/Core/Providers/JsonReader.cs
@@ -0,0 +1,28 @@
+﻿using MovieTheater.Framework.Core.Contracts;
+using System.IO;
+
+namespace MovieTheater.Framework.Core.Providers
+{
+    public class JsonReader : IReader
+    {
+        public JsonReader(IReader reader, IWriter writer)
+        {
+            this.Reader = reader;
+            this.Writer = writer;
+        }
+
+        public IReader Reader { get; private set; }
+        public IWriter Writer { get; private set; }
+
+        public string Read()
+        {
+            this.Writer.Write("Enter path of JSON file:");
+            string path = this.Reader.Read();
+
+            StreamReader readJson = new StreamReader(path);
+            string jsonString = readJson.ReadToEnd();
+
+            return jsonString;
+        }
+    }
+}
