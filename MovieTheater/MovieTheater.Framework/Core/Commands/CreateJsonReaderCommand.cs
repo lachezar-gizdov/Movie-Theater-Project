05e6c3ef6a0b030234922736c36b4f3cef8f097a
Encho Enevski <encho.enevski@gmail.com>
2017-06-08 18:48:57 +0300

    Create JSon Reader command is ready

--- MovieTheater/MovieTheater.Framework/Core/Commands/CreateJsonReaderCommand.cs
+++ MovieTheater/MovieTheater.Framework/Core/Commands/CreateJsonReaderCommand.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MovieTheater.Framework.Core.Commands.Contracts;
+using MovieTheater.Framework.Core.Providers;
+using MovieTheater.Framework.Core.Providers.Contracts;
 
 namespace MovieTheater.Framework.Core.Commands
 {
     public class CreateJsonReaderCommand : ICommand
     {
+        private IReader reader;
+        private IWriter writer;
+
+        public CreateJsonReaderCommand()
+        {
+            this.reader = new ConsoleReader();
+            this.writer = new ConsoleWriter();
+        }
+
         public string Execute(List<string> parameters)
         {
-            throw new NotImplementedException();
+            var jsonReader = new JsonReader(reader, writer);
+
+            jsonReader.Read();
+
+            return "Successfully read json file!";
         }
     }
 }
