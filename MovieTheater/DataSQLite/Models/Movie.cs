0139ebee5c0c08d6371db82c650c9ce90fd3ccb3
olebg <olebg01@gmail.com>
2017-06-10 01:13:13 +0300

    add working SQLite database, needs refactoring

---
+++ MovieTheater/DataSQLite/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace DataSQLite.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public short Year { get; set; }
+
+        public virtual Genre Genre { get; set; }
+    }
+}
---
+++ MovieTheater/DataSQLite/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using DataSQLite.Models;
+using DataSQLite.YourDbMigrations;
+using System.Data.Entity;
+
+namespace DataSQLite
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<SQLDbContext, AwesomeConfig>());
+            var data = new SQLDbContext();
+
+            var genre = new Genre() { Name = "Comedy" };
+            var movie = new Movie() { Name = "Movie", Genre = genre };
+
+            data.Movies.Add(movie);
+            data.SaveChanges();
+        }
+    }
+}
