c1eacd2e9298328aa1c3ae9bdfde3fe4c781a0cc
Iskren Donev <iskren.donev@me.com>
2017-06-11 19:54:20 +0300

    Updated the Movie model

--- MovieTheater/MovieTheater.Models/Movie.cs
+++ MovieTheater/MovieTheater.Models/Movie.cs
@@ -1,15 +1,45 @@
-﻿namespace MovieTheater.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieTheater.Models
 {
     public class Movie
     {
+        private const short FIRST_FEATURE_FILM_YEAR = 1906;
+
+        private short year;
+
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public short Year { get; set; }
+        public short Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Cannot have a non-positive year!");
+                }
+                else if (value < FIRST_FEATURE_FILM_YEAR)
+                {
+                    throw new ArgumentException("The entered date precedes any known feature films!");
+                }
+                else
+                {
+                    this.year = value;
+                }
+            }
+        }
 
+        [Required]
         public virtual Theater Theater { get; set; }
 
-        public virtual Ticket Ticket { get; set; }
+        [Required]
+        public virtual Ticket Tickets { get; set; }
     }
 }
