ce3d91d05faf310df66caf3298836f55b41f9304
olebg <olebg01@gmail.com>
2017-06-12 23:58:55 +0300

    fix create new user command info in helper

--- MovieTheater/MovieTheater.Framework/Core/Commands/Common/DisplayHelpCommand.cs
+++ MovieTheater/MovieTheater.Framework/Core/Commands/Common/DisplayHelpCommand.cs
@@ -2,11 +2,8 @@
 using MovieTheater.Framework.Core.Commands.Abstractions;
 using MovieTheater.Framework.Core.Commands.Contracts;
 using MovieTheater.Models.Factory.Contracts;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MovieTheater.Framework.Core.Commands.Common
 {
@@ -24,7 +21,7 @@ namespace MovieTheater.Framework.Core.Commands.Common
             output.AppendLine("Below is the full list of supported commands:");
             output.AppendLine(" - Type \"createtheater [theater name] [city name]\" to create a new theater.");
             output.AppendLine(" - Type \"createmovie [movie title] [movie year] [movie duration] [movie director]\" to create a new theater.");
-            output.AppendLine(" - Type \"createuser [first name] [last name] [city]");
+            output.AppendLine(" - Type \"createuser [first name] [last name] [city]\" to create a new user");
             output.AppendLine(" - Type \"exit\" to quit the program.");
 
             return output.ToString();
