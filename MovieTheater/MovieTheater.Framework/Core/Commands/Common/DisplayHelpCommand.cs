553ec5eff78d8962d5a4176a0a08df95546886f3
Lucho Gizdov <olebg01@gmail.com>
2017-06-13 15:25:37 +0300

    add commands in the helper

--- MovieTheater/MovieTheater.Framework/Core/Commands/Common/DisplayHelpCommand.cs
+++ MovieTheater/MovieTheater.Framework/Core/Commands/Common/DisplayHelpCommand.cs
@@ -20,8 +20,11 @@ namespace MovieTheater.Framework.Core.Commands.Common
 
             output.AppendLine("Below is the full list of supported commands:");
             output.AppendLine(" - Type \"createtheater [theater name] [city name]\" to create a new theater.");
-            output.AppendLine(" - Type \"createmovie [movie title] [movie year] [movie duration] [movie director]\" to create a new theater.");
+            output.AppendLine(" - Type \"createmovie [movie title] [movie year] [movie duration] [movie director]\" to create a new movie.");
+            output.AppendLine(" - Type \"createticket [Movie Name]  [Hall Id] [Price] [User Name] [Seat Number]\" to create a new ticket");
             output.AppendLine(" - Type \"createuser [first name] [last name] [city]\" to create a new user");
+            output.AppendLine(" - Type \"createhall [Hall Number]  [Theater]\" to create a new hall");
+            output.AppendLine(" - Type \"createhallschedule [Hall Number]  [Hall] [Projection Time](optional)\" to create a new hall schedule");
             output.AppendLine(" - Type \"exit\" to quit the program.");
 
             return output.ToString();
